6b0cfdb873f8d6e83317f4cf6bccf60e34606a07
hanxiao <zhuec@hotmail.com>
2019-05-21 12:19:57 +0800

    refactor: 优化进度条显示样式

--- lib/screen/feed_screen.dart
+++ lib/screen/feed_screen.dart
@@ -106,12 +106,29 @@ class _FeedScreenState extends State<FeedScreen>
   double _progress = 0;
 
   Widget _progressIndicator(double value) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 7.0),
+    // return DecoratedBox(
+    //   child: Padding(
+    //     child: SizedBox(
+    //       height: 2.0,
+    //       child: new LinearProgressIndicator(value: value),
+    //     ),
+    //     padding: EdgeInsets.symmetric(horizontal: 7.0),
+    //   ),
+    //   decoration: BoxDecoration(
+    //     borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    //   ),
+    // );
+    return Padding(
       child: SizedBox(
-        height: 2.0,
-        child: new LinearProgressIndicator(value: value),
+        height: 3.0,
+        child: PhysicalModel(
+          child: LinearProgressIndicator(value: value),
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(80.0),
+          clipBehavior: Clip.antiAlias,
+        ),
       ),
+      padding: EdgeInsets.symmetric(horizontal: 7.0),
     );
   }
 

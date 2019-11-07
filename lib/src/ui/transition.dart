adedb064bd228c7b8b6f47ec4d0e17d9cca55cfc
dinn <dinn.ding@vechain.com>
2019-11-07 17:36:25 +0800

    fix

--- lib/src/ui/transition.dart
+++ lib/src/ui/transition.dart
@@ -2,30 +2,16 @@ import 'package:flutter/cupertino.dart';
 
 Future<dynamic> slide(BuildContext context, Widget widget,
     {String routeName}) async {
-  PageRouteBuilder pageRouteBuilder;
-  if (routeName != null) {
-    pageRouteBuilder = PageRouteBuilder(
-      barrierDismissible: false,
-      transitionDuration: Duration(milliseconds: 200),
-      pageBuilder: (context, a, b) {
-        return SlideTransition(
-          position: Tween(begin: Offset(0, 1), end: Offset.zero).animate(a),
-          child: widget,
-        );
-      },
-      settings: RouteSettings(name: routeName),
-    );
-  } else {
-    pageRouteBuilder = PageRouteBuilder(
-      barrierDismissible: false,
-      transitionDuration: Duration(milliseconds: 200),
-      pageBuilder: (context, a, b) {
-        return SlideTransition(
-          position: Tween(begin: Offset(0, 1), end: Offset.zero).animate(a),
-          child: widget,
-        );
-      },
-    );
-  }
+  PageRouteBuilder pageRouteBuilder = PageRouteBuilder(
+    barrierDismissible: false,
+    transitionDuration: Duration(milliseconds: 150),
+    pageBuilder: (context, a, b) {
+      return SlideTransition(
+        position: Tween(begin: Offset(0, 1), end: Offset.zero).animate(a),
+        child: widget,
+      );
+    },
+    settings: routeName == null ? null : RouteSettings(name: routeName),
+  );
   return Navigator.of(context).push(pageRouteBuilder);
 }

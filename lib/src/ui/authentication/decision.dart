6283794faa56f27ec22d6ed38f66e8b042094c62
hanxiao <zhuec@hotmail.com>
2019-11-25 14:09:08 +0800

    feat: go to system setting

--- lib/src/ui/authentication/decision.dart
+++ lib/src/ui/authentication/decision.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_cracker/flutter_bloc_cracker.dart';
 import 'package:veatre/src/ui/authentication/bloc/bloc.dart';
 import 'package:veatre/src/ui/authentication/bloc/event.dart';
 import 'package:veatre/src/ui/authentication/bloc/state.dart';
 import 'package:veatre/src/ui/authentication/unlock.dart';
+import 'package:veatre/src/ui/commonComponents.dart';
 import 'package:veatre/src/ui/mainUI.dart';
+import 'package:system_setting/system_setting.dart';
 
 class Decision extends StatefulWidget {
   Decision({Key key, this.canCancel: false}) : super(key: key);
@@ -30,8 +31,17 @@ class _DecisionState extends State<Decision> {
     super.dispose();
   }
 
+  void _subscribeBloc() => _bloc.state.listen((state) {
+        if (state is Unauthenticated && state.authType == AuthType.biometrics)
+          _redirectToAuthenticate(state);
+
+        if (state is Authenticated && state.didAuthenticate)
+          _redirectToMainUi();
+      });
+
   @override
   Widget build(BuildContext context) {
+    final Color primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
       body: SafeArea(
         maintainBottomViewPadding: true,
@@ -56,10 +66,32 @@ class _DecisionState extends State<Decision> {
                 initialData: _bloc.initialState,
                 builder: (context, snapshot) => Expanded(
                   child: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 500),
+                    duration: const Duration(milliseconds: 200),
                     child: _isShowUnlockWidget(snapshot.data)
                         ? Unlock(canCancel: widget.canCancel)
-                        : Container(),
+                        : Column(
+                            children: <Widget>[
+                              SizedBox(height: 222),
+                              FlatButton(
+                                child: Column(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.fingerprint,
+                                      size: 65,
+                                      color: primaryColor,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      '点击进行物理识别',
+                                      style: TextStyle(color: primaryColor),
+                                    )
+                                  ],
+                                ),
+                                onPressed: () =>
+                                    _redirectToAuthenticate(snapshot.data),
+                              ),
+                            ],
+                          ),
                   ),
                 ),
               ),
@@ -71,27 +103,30 @@ class _DecisionState extends State<Decision> {
   }
 
   bool _isShowUnlockWidget(AuthenticationState state) {
-    if (state is Unauthenticated &&
-        state.authenticationType == AuthenticationType.password) return true;
+    if (state is Unauthenticated && state.authType == AuthType.password)
+      return true;
     if (state is Authenticated && !state.didAuthenticate) return true;
     return false;
   }
 
-  void _subscribeBloc() => _bloc.state.listen((state) {
-        if (state is Unauthenticated &&
-            state.authenticationType == AuthenticationType.biometrics)
-          _redirectToAuthenticate();
+  void _redirectToAuthenticate(Unauthenticated state) =>
+      state.hasAuthority ? _authenticate() : _applySystemAuthority();
 
-        if (state is Authenticated && state.didAuthenticate)
-          _redirectToMainUi();
-      });
-
-  void _redirectToAuthenticate() =>
-      WidgetsBinding.instance.addPostFrameCallback(
+  void _authenticate() => WidgetsBinding.instance.addPostFrameCallback(
         (_) => Future.delayed(
           Duration(milliseconds: 200),
           () => _bloc.emit(Authenticate()),
         ),
+      );
+
+  void _applySystemAuthority() => customAlert(
+        context,
+        title: Text('物理识别'),
+        content: Text('去设置物理识别权限'),
+        confirmAction: () async {
+          SystemSetting.goto(SettingTarget.LOCATION);
+          Navigator.of(context).pop();
+        },
       );
 
   void _redirectToMainUi() {
